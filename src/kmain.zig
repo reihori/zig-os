a5991fa6db0df9bd0249e034d112bc499ad0d0de
reihori <151887312+reihori@users.noreply.github.com>
2024-10-19 17:38:26 +0900

    vga

--- src/kmain.zig
+++ src/kmain.zig
@@ -20,5 +20,36 @@ export fn _start() noreturn {
 }
 
 fn kmain() noreturn {
+    vgaWriteString("Hello, World!", 0, 0, .lightred);
     while (true) {}
+}
+
+const VgaColor = enum(u8) {
+    black = 0,
+    blue = 1,
+    green = 2,
+    cyan = 3,
+    red = 4,
+    magenta = 5,
+    brown = 6,
+    lightgrey = 7,
+    darkgrey = 8,
+    lightblue = 9,
+    lightgreen = 10,
+    lightcyan = 11,
+    lightred = 12,
+    lightmagenta = 13,
+    yellow = 14,
+    white = 15,
+};
+
+fn vgaWriteChar(char: u8, x: usize, y: usize, color: VgaColor) void {
+    const vgaBuffer: [*]volatile u16 = @ptrFromInt(0xB8000);
+    vgaBuffer[y * 80 + x] = @as(u16, @intFromEnum(color)) << 8 | @as(u16, char);
+}
+
+fn vgaWriteString(string: []const u8, x: usize, y: usize, color: VgaColor) void {
+    for (string, 0..) |char, i| {
+        vgaWriteChar(char, x + i, y, color);
+    }
 }
